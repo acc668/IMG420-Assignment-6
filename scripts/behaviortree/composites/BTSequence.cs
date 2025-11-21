90ca5d9171e0619b196e256f4a405adc8db81025
acc668 <116805246+acc668@users.noreply.github.com>
2025-11-21 00:39:24 -0700

    Add files via upload

---
+++ scripts/behaviortree/composites/BTSequence.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public partial class BTSequence : BTNode
+{
+    private int currentChildIndex = 0;
+
+    public override BTState Tick(float delta)
+    {
+        for (int i = currentChildIndex; i < GetChildCount(); i++)
+        {
+            var child = GetChild<BTNode>(i);
+            var state = child.Tick(delta);
+
+            if (state == BTState.Running)
+            {
+                currentChildIndex = i;
+                return BTState.Running;
+            }
+            else if (state == BTState.Failure)
+            {
+                Reset();
+                return BTState.Failure;
+            }
+        }
+
+        Reset();
+        return BTState.Success;
+    }
+
+    public override void Reset()
+    {
+        currentChildIndex = 0;
+        foreach (var child in GetChildren())
+        {
+            if (child is BTNode btNode)
+                btNode.Reset();
+        }
+    }
+}
